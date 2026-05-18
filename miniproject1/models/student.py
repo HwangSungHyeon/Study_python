196d3a5a57d8fec0a3205bbae593952e76c6bff6
HwangSungHyeon <asyoung316@gmail.com>
2026-05-18 17:27:19 +0900

    26.05.18 미니프로젝트

---
+++ miniproject1/models/student.py
@@ -0,0 +1,13 @@
+
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from database.db import Base
+
+class Student(Base):
+    __tablename__ = "student"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False) # nullable=False : null값 넣지 않겠다.
+    class_n = Column(String(100))
+    team = Column(String(100))
+    create_at = Column(DateTime, default=datetime.now)
